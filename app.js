39e1f959da51a8ff3fbf9b9dbb2e7aefccd30a07
Darkx23312 <130650239+Darkx-dev@users.noreply.github.com>
2024-03-02 03:44:05 +0530

    main

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const inputFieldsFirst = document.querySelectorAll("input")
+const nextButton = document.querySelector(".nextBtn")
+const errorSpans = document.querySelectorAll(".errorSpan")
+
+let validity = {
+    stepOne: {
+        name: false,
+        email: false,
+        phone: false
+    }
+}
 
 // toggles error 
 const toggleError = (flag, element, errorText) => {
@@ -14,29 +24,69 @@ const handleNextStep = () => {
     console.log("Hello world!")
 }
 
+const enableButton = () => {
+    nextButton.classList.add("enabled")
+    nextButton.classList.remove("disabled")
+    nextButton.disabled = false
+}
+
+const disableButton = () => {
+    nextButton.disabled = true
+    nextButton.classList.add("disabled")
+    nextButton.classList.remove("enabled")
+}
+
+const toggleButtonBehaviour = () => {
+    if (validity.stepOne.name === true && validity.stepOne.email === true && validity.stepOne.phone === true) {
+        enableButton()
+    } else {
+        disableButton()
+    }
+
+}
+
 // KeyUp evemts on input tag
 inputFieldsFirst.forEach((input, index) => {
     input.addEventListener("keyup", (e) => {
         let errorSpan = e.target.parentElement.childNodes[1].childNodes[3]
         let targetValue = e.target.value
         if (index === 0) {
-            if (targetValue == "" || null) toggleError(true, errorSpan, "This field is required")
-            else toggleError(false, errorSpan)
+            if (targetValue == "" || null) {
+                validity.stepOne.name = false
+                toggleError(true, errorSpan, "This field is required")
+            }
+            else {
+                validity.stepOne.name = true
+                toggleError(false, errorSpan)
+            }
         }
 
         if (index === 1) {
-            if (targetValue.includes('@') && targetValue.includes('.')) toggleError(false, errorSpan)
-            else toggleError(true, errorSpan, "This field is required")
+            if (targetValue.includes('@') && targetValue.includes('.')) {
+                validity.stepOne.email = true
+                toggleError(false, errorSpan)
+            }
+            else {
+                validity.stepOne.email = false
+                toggleError(true, errorSpan, "This field is required")
+            }
         }
 
         if (index === 2) {
             if (targetValue.length == 0) {
+                validity.stepOne.phone = false
                 toggleError(true, errorSpan, "This field is required")
             }
             else if (targetValue.length < 10) {
+                validity.stepOne.phone = false
                 toggleError(true, errorSpan, "Atleast 10 numbers")
             }
-            else toggleError(false, errorSpan)
+            else {
+                validity.stepOne.phone = true
+                toggleError(false, errorSpan)
+            }
         }
+
+        toggleButtonBehaviour()
     })
 })
